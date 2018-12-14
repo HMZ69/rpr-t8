5c2209eb8e416f0584fa60fcc1ef18b7cf4c2fc4
Hamza Muminović <44194330+HMZ69@users.noreply.github.com>
2018-12-14 13:59:21 +0100

    Zadatak 1

--- src/ba/unsa/etf/rpr/Controller.java
+++ src/ba/unsa/etf/rpr/Controller.java
@@ -3,16 +3,20 @@ package ba.unsa.etf.rpr;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
+import javax.print.DocFlavor;
 import java.io.File;
 import java.io.FilenameFilter;
+import java.net.URL;
+import java.util.Objects;
+import java.util.ResourceBundle;
 
 
-public class Controller {
-
-    private ObservableList<String> pronadjeno = FXCollections.observableArrayList();
+public class Controller implements Initializable {
 
     @FXML
     private TextField input;
@@ -20,19 +24,60 @@ public class Controller {
     @FXML
     private ListView<String> output;
 
+    @FXML
+    private Button traziBtn;
+
+    @FXML
+    private Button prekiniBtn;
+
+    private static boolean prekini = false;
+    private Thread thread;
+    private ObservableList<String> pronadjeno = FXCollections.observableArrayList();
+
     public void pretraga(File dir) {
-        for (File f : dir.listFiles()) {
-            if (f.isDirectory())
-                pretraga(f);
-            else if (f.isFile() && f.getName().contains(input.getText())) {
-                pronadjeno.add(f.toString());
-                output.setItems(pronadjeno);
+        try {
+            if (prekini)
+                return;
+            for (File f : dir.listFiles()) {
+                if (f.isDirectory())
+                    pretraga(f);
+                else if (f.isFile() && f.getName().contains(input.getText())) {
+                    pronadjeno.add(f.toString());
+                    output.setItems(pronadjeno);
+                }
             }
+        }
+        catch(Exception e) {
+            //do nothing
         }
     }
 
     public void traziBtn() {
+        prekini = false;
         File f = new File(System.getProperty("user.home"));
-        pretraga(f);
+        try {
+            thread = new Thread( () -> {
+                prekiniBtn.setDisable(false);
+                traziBtn.setDisable(true);
+                pretraga(f);
+                prekiniBtn.setDisable(true);
+                traziBtn.setDisable(false);
+            });
+        }
+        catch (Exception e) {
+            //do nothing
+        }
+
+        thread.start();
+    }
+
+    public void prekiniBtn() {
+        prekini = true;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        prekiniBtn.setDisable(true);
+        traziBtn.setDisable(false);
     }
 }
--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -1,9 +1,11 @@
 package ba.unsa.etf.rpr;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class Main extends Application {
